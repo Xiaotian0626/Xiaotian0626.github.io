f2fb938d81c9dc2c4cdaf13b4b08fb4c88e902a9
Xiaotian Li <lixiaotiane@outlook.com>
2020-10-09 21:30:12 +0800

    Updates

---
+++ precache-manifest.e8126e6ae48451ebf327433284861f9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e68ff5872e51905eb152963205fcbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7b5d04f197bfc471dcf",
+    "url": "/static/css/main.4dbc434b.chunk.css"
+  },
+  {
+    "revision": "b44d0157ee1bae4c313b",
+    "url": "/static/js/2.f078ebf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f078ebf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b5d04f197bfc471dcf",
+    "url": "/static/js/main.e3154db8.chunk.js"
+  },
+  {
+    "revision": "5755b401f8c885843eca",
+    "url": "/static/js/runtime-main.0adea72d.js"
+  }
+]);
